09680374d21a955269a4940144847d21e554dab9
bingo8670 <307286130@qq.com>
2017-07-26 19:39:15 +0800

    4-5 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
 
   has_many :likes, :dependent => :destroy
   has_many :liked_posts, :through => :likes, :source => :post
-  
+
   def display_name
     # # 取email的前半部分来显示，如果你也可以另开一个字段是nickname让用户可以自己编辑显示名称
     self.email.split("@").first
+  end
+
+  def is_admin?
+    role == "admin"
   end
 
 end
